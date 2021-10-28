3d43ecd3a90e644eed97e83efbd31df587706724
Chen Lay <maungmaungyoker.mmy@gmail.com>
2021-10-28 20:45:55 +0630

    My First Commit :fire:

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer')
+const cron = require('node-cron')
+require('dotenv').config()
+
+const baseUrl = process.env.BASE_URL
+
+// * Start my script
+async function start() {
+	const browser = await puppeteer.launch()
+
+	const page = await browser.newPage()
+
+	await page.goto(baseUrl)
+
+	// TODO: This is the best way that I found it on the Planet
+	await Promise.all([page.click('#submitForm'), page.waitForNavigation()])
+
+	await page.$eval('#submitForm', (el) => console.log(el.textContent))
+
+	await browser.close()
+}
+
+// * Execute Function
+cron.schedule('32 16 * * 1-5', start)
--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	reactStrictMode: true,
-	env: {
-		myApi: process.env.NEXT_PUBLIC_BASE_URL,
-		apiKey: process.env.NEXT_PUBLIC_MY_API_KEY,
-	},
-}
